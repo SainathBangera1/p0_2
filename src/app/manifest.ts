e4a9adc8eedaf1d1c11cfa73fd2272e33e659a94
SainathBangera1 <psainathbangera@gmail.com>
2025-05-24 16:31:01 +0530

    10 commit - changing display to fullscreen in manifest file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description:
       'a simple calculator that performs addition, substraction, multiplication and division',
     start_url: '/',
-    display: 'standalone',
+    display: 'fullscreen',
     background_color: 'white',
     theme_color: 'white',
     icons: [
