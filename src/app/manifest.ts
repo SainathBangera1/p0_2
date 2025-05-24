b75a010c9fa4bed1ca79e45cba61dd4781391ddc
SainathBangera1 <psainathbangera@gmail.com>
2025-05-24 15:55:19 +0530

    7th commit upadte pwa icons in manifest file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,35 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: 'white',
     icons: [
       {
-        src: '/favicon.ico',
+        src: '/android-chrome-192x192.png', // Path to a 192x192 icon
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png', // Path to a 512x512 icon
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-32x32.png', // Standard favicon size
         sizes: '32x32',
-        type: 'image/x-icon',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-16x16.png', // Smaller favicon size
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png', // Apple touch icon for iOS devices
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/maskable_icon.png', // A maskable icon for Android adaptive icons
+        sizes: 'any', // Or specify common sizes like '512x512'
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
